87d8cebc7127f65dd416ad70d60e878ed2f491be
Raven Dee <ravendeedelacruz19@gmail.com>
2023-12-07 23:24:40 +0800

    Pre-final commit

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,3 +1,17 @@
-export default function Home() {
-	return <></>;
-}
+import Hero from "./Hero";
+import WeekSpecials from "./WeekSpecials";
+import Testimonials from "./Testimonials";
+import OurStory from "./OurStory";
+
+const Home = () => {
+	return (
+		<>
+			<Hero />
+			<WeekSpecials />
+			<Testimonials />
+			<OurStory />
+		</>
+	);
+};
+
+export default Home;
