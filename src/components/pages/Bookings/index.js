730b003a0f4bb7f4acb9132cc85306af7f8c6191
Raven Dee <ravendeedelacruz19@gmail.com>
2023-12-07 22:20:35 +0800

    Add booking page

---
+++ src/components/pages/Bookings/index.js
@@ -0,0 +1,34 @@
+import { useReducer } from "react";
+import { useNavigate } from "react-router-dom";
+import "./index.css";
+import BookingForm from "./BookingForm";
+import { fetchAPI, submitAPI } from "../../../utils/fakeAPI";
+import pages from "../../../utils/pages";
+
+const updateTimes = (availableTimes, date) => {
+	const response = fetchAPI(new Date(date));
+	return response.length !== 0 ? response : availableTimes;
+};
+
+const initializeTimes = (initialAvailableTimes) => [...initialAvailableTimes, ...fetchAPI(new Date())];
+
+export default function Bookings() {
+	const [availableTimes, dispatchOnDateChange] = useReducer(updateTimes, [], initializeTimes);
+	const navigate = useNavigate();
+
+	const submitData = (formData) => {
+		const response = submitAPI(formData);
+		if (response) navigate(pages.get("confirmedBooking").path);
+	};
+
+	return (
+		<div className="bookings">
+			<h2>Table reservation</h2>
+			<BookingForm
+				availableTimes={availableTimes}
+				dispatchOnDateChange={dispatchOnDateChange}
+				submitData={submitData}
+			/>
+		</div>
+	);
+}
